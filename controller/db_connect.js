31062521492e8f451f6315181ce3edd4039dd8d3
asaeed0 <a.saeed@mail.utoronto.ca>
2018-09-13 18:44:29 -0400

    updates mongo creds

--- controller/db_connect.js
+++ controller/db_connect.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //  Establishes connection to Mongo and Specific database both from .env
-mongoose.connect( process.env.MONGODB_URI, {dbName: process.env.DB_NAME, useNewUrlParser: true }, (err) => {
-    if(err) throw err;
-    console.log(`Connected to Database: ${process.env.DB_NAME}`);
-});
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true },
+  err => {
+    if (err) throw err;
+    console.log(`Connected to Database`);
+  }
+);
